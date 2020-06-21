61e4b2370774649a1a4e1505e7acdf66c8776294
Happollyon <36013973+Happollyon@users.noreply.github.com>
2020-06-21 11:35:51 +0100

    added requirements fille

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-URL= 'https://www.agazeta.com.br/'
+# go to https://www.guerrillamail.com/
+# generate a randon email
+# generate randon name, randon surname, randon bday
+# save to database
+# go to signup page -> 
+# enter name, email, birthday date
+# generate password -> save to database
+# next-> next-> sign up->get code from email -> and finish#signing up 
+# get an api key
+
+URL= 'https://twitter.com/i/flow/signup'
 page= requests.get(URL)
 
 soup = BeautifulSoup(page.content,'html.parser')
 
 print(page)
 print('')
-print(search)
+print(soup)
