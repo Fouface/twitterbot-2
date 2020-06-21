e5d8ae40ff5223523c4c7556cff5a688a543ac3b
Happollyon <36013973+Happollyon@users.noreply.github.com>
2020-06-21 14:16:15 +0100

    added selenium to the project

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-import requests
+from selenium import webdriver
+import requests, random, string
 from bs4 import BeautifulSoup
+
+
+
 # go to https://www.guerrillamail.com/
 # generate a randon email
 # generate randon name, randon surname, randon bday
@@ -10,11 +14,24 @@ from bs4 import BeautifulSoup
 # next-> next-> sign up->get code from email -> and finish#signing up 
 # get an api key
 
+
+#this function creates a randon string to be the email
+def randomword(length):
+    letters=    string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+random_email = randomword(8)
+
+
+driver = webdriver.Chrome(executable_path='/Users/siber/Desktop\chromedriver.exe')
+driver.get('https://guerrillamail.com/')
+element= driver.find_element_by_id( "inbox-id")
+element.click()
+
+# driver.quit()
 URL= 'https://twitter.com/i/flow/signup'
 page= requests.get(URL)
 
 soup = BeautifulSoup(page.content,'html.parser')
 
-print(page)
-print('')
-print(soup)
